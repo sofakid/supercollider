sofakid/supercollider

commit 3724b47e86c21c934253b3e71f2ceeaf879bb63b

scide: Improve ability to distinguish which split is active
The IDE uses a single tab bar for all splits. This means it is often difficult to see which split corresponds to the active tab. This commit provides more visual feedback, indicating which split has focus, and matching the background colour of the number list to those used in active and inactive tabs as appropriate.

Signed-off-by: Scott Wilson <i@scottwilson.ca>

editors/sc-ide/widgets/code_editor/editor.cpp
@@ -55,7 +55,9 @@ GenericCodeEditor::GenericCodeEditor( Document *doc, QWidget *parent ):
 {
     Q_ASSERT(mDoc != 0);
 
-    setFrameShape( QFrame::NoFrame );
+    setLineWidth(1);
+    setFrameShape(QFrame::Box);
+    setFrameShadow(QFrame::Raised);
 
     viewport()->setAttribute( Qt::WA_MacNoClickThrough, true );
 
@@ -711,6 +713,18 @@ void GenericCodeEditor::wheelEvent( QWheelEvent * e )
     QPlainTextEdit::wheelEvent(e);
 #endif
 }
+    
+void GenericCodeEditor::focusInEvent( QFocusEvent * e )
+{
+    setFrameShadow(QFrame::Sunken);
+    setFrameShape(QFrame::Box);
+}
+    
+void GenericCodeEditor::focusOutEvent( QFocusEvent * e )
+{
+    setFrameShape(QFrame::Box);
+    setFrameShadow(QFrame::Raised);
+}
 
 void GenericCodeEditor::dragEnterEvent( QDragEnterEvent * event )
 {
@@ -851,12 +865,20 @@ void GenericCodeEditor::paintLineIndicator( QPaintEvent *e )
     QRect r( e->rect() );
     QPainter p( mLineIndicator );
 
-    p.fillRect( r, plt.color( QPalette::Mid ) );
+    if(hasFocus()) {
+        p.fillRect( r, plt.color( QPalette::Midlight ) );
+    } else {
+        p.fillRect( r, plt.color( QPalette::Mid ) );
+    }
+
     p.setPen( plt.color(QPalette::Dark) );
     p.drawLine( r.topRight(), r.bottomRight() );
 
-    p.setPen( plt.color(QPalette::ButtonText) );
-
+    if(hasFocus()) {
+        p.setPen( plt.color(QPalette::ButtonText) );
+    } else {
+        p.setPen( plt.color(QPalette::Midlight) );
+    }
     QTextDocument *doc = QPlainTextEdit::document();
     QTextCursor cursor(textCursor());
     int selStartBlock, selEndBlock;

editors/sc-ide/widgets/code_editor/editor.hpp
@@ -63,6 +63,8 @@ protected:
     virtual void mouseReleaseEvent( QMouseEvent * );
     virtual void wheelEvent( QWheelEvent * );
     virtual void dragEnterEvent( QDragEnterEvent * );
+    virtual void focusInEvent( QFocusEvent * );
+    virtual void focusOutEvent( QFocusEvent * );
     void hideMouseCursor(QKeyEvent *);
     virtual QMimeData *createMimeDataFromSelection() const;
 

commit 7e2b769ac3b0e6f81827724d408a70b57067cc33

scide: Fix Bug introduced by commit 3724b47
Forgot to call superclass focus methods

Signed-off-by: Scott Wilson <i@scottwilson.ca>

editors/sc-ide/widgets/code_editor/editor.cpp
@@ -716,12 +716,14 @@ void GenericCodeEditor::focusInEvent( QFocusEvent * e )
 {
     setFrameShadow(QFrame::Sunken);
     setFrameShape(QFrame::Box);
+    QPlainTextEdit::focusInEvent(e);
 }
     
 void GenericCodeEditor::focusOutEvent( QFocusEvent * e )
 {
     setFrameShape(QFrame::Box);
     setFrameShadow(QFrame::Raised);
+    QPlainTextEdit::focusOutEvent(e);
 }
 
 void GenericCodeEditor::dragEnterEvent( QDragEnterEvent * event )